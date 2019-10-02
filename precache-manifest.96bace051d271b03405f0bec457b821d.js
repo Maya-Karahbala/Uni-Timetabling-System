091a7b0fbb3b688a50be461817a9e33803667325
maya <maya.karabala8@gmail.com>
2019-10-02 14:15:38 +0300

    Updates

---
+++ precache-manifest.96bace051d271b03405f0bec457b821d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6871c7d2cc056c18c08db750cbb142",
+    "url": "/Uni-Timetabling-System/index.html"
+  },
+  {
+    "revision": "df429d5b0fb5fdaa1775",
+    "url": "/Uni-Timetabling-System/static/css/main.c5e4a391.chunk.css"
+  },
+  {
+    "revision": "87b2d1cae5a5ff6c8805",
+    "url": "/Uni-Timetabling-System/static/js/2.87952853.chunk.js"
+  },
+  {
+    "revision": "df429d5b0fb5fdaa1775",
+    "url": "/Uni-Timetabling-System/static/js/main.ee32b2f5.chunk.js"
+  },
+  {
+    "revision": "15406b867988e988371f",
+    "url": "/Uni-Timetabling-System/static/js/runtime-main.c0da76a6.js"
+  }
+]);
